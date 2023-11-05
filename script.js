9c1ea1ded81ce9a9bd85038da1738c05632331cd
TBVitale <148408852+TBVitale@users.noreply.github.com>
2023-11-05 17:30:57 -0300

    nuevos cambios

--- script.js
+++ script.js
@@ -1,25 +1,52 @@
-let video=document.querySelector('video');
-let tiempoActual;
-let botonInicio= document.querySelector("#play");
-let botonPause= document.querySelector("#pause");
-let tiempoVideo = document.querySelector(".duracion-video");
 
-function segundosAMinutosYSegundos(segundos) {
+let arreglo = ["", "", ""]
+  let video = document.querySelector('video');
+  let tiempoActual;
+  let botonInicio = document.querySelector("#play");
+  let botonPause = document.querySelector("#pause");
+  let tiempoVideo = document.querySelector(".duracion-video");
+
+  function segundosAMinutosYSegundos(segundos) {
     const minutos = Math.floor(segundos / 60);
     const segundosRestantes = Math.floor(segundos % 60);
     return `${minutos}:${segundosRestantes}`;
   }
 
-botonInicio.addEventListener('click',()=>{
+  botonInicio.addEventListener('click', () => {
     video.play()
-    tiempoActual=setInterval(()=>{
-    const tiempoActualEnSegundos = video.currentTime;
-    const tiempoActualFormateado = segundosAMinutosYSegundos(tiempoActualEnSegundos);
-    tiempoVideo.textContent = `Tiempo actual: ${tiempoActualFormateado}`;
-  }, 1000);
-});
+    tiempoActual = setInterval(() => {
+      const tiempoActualEnSegundos = video.currentTime;
+      const tiempoActualFormateado = segundosAMinutosYSegundos(tiempoActualEnSegundos);
+      tiempoVideo.textContent = `Tiempo actual: ${tiempoActualFormateado}`;
+    }, 1000);
+  });
 
-botonPause.addEventListener('click',()=>{
+
+  botonPause.addEventListener('click', () => {
     video.pause()
     clearInterval(tiempoActual)
-});
+  });
+
+  /*PUZZLE*/
+
+  
+
+  function allowDrop(ev) {
+    ev.preventDefault();
+  }
+
+  function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+  }
+
+  function drop(ev) {
+    if (arreglo[parseInt(ev.target.id)] == "") {
+      var data = ev.dataTransfer.getData("text");
+      arreglo[parseInt(ev.target.id)] = data;
+      ev.target.appendChild(document.getElementById(data))
+    }
+  }
+
+  function buttonReload() {
+    window.location.reload()
+  }
